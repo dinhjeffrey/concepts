71ef988eb2c3d2f494b34483726fb0ca13ee0748
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-19 11:55:08 -0700

    added summary.java. added representing complex types with classes

--- objectModel.js
+++ objectModel.js
@@ -1,1 +1,8 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Details_of_the_Object_Model
+// In JS you can create a new constructor using the operator "new" to create a new object
+
+function Employee() {
+	this.name = "";
+	this.dept = "general";
+}
+
