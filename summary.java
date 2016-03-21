3882aa1ef0c41416fed369d7eeadec8f0f32f042
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-20 18:13:59 -0700

    test

---
+++ summary.java
@@ -0,0 +1,12 @@
+/* Representing Complex Types with Classes
+A class is a template for creating an object. The way we describe an object.
+- Declared with class keyword
+- Class instances (a.k.a. objects) allocated with 'new' keyword
+Class are reference types. Variables don't hold classes themselves but a reference to the class instance. Very different from primitive types
+Use access modifiers to control encapsulation. Dont want our details exposed to the users of our objects.
+Mthods manipulate state and perform operations
+- Use return keyword to exit and/or return a value
+Field store object state
+- Interaction normally controlled through accessors(getters) and mutators(setters). Use methods to change data of fields without affecting usage of our class.
+
+*/
