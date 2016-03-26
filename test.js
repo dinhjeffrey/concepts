098a8e109efd57561eb14667817f09cf1b300924
dinhjeffrey <dinhjeffreyy@gmail.com>
2016-03-26 10:46:42 -0700

    added copy scope exercises

---
+++ test.js
@@ -0,0 +1,15 @@
+function blender(fruit) {
+	var f = fruit
+	var y = 'yogurt'
+	function fs(){
+	var x = 'asdf'
+	console.log(f + ' and ' + y + ' makes ' + f + ' swirl' )
+	}
+	fs()
+}
+blender('blueberry')
+
+// var add = function (a,b) {
+// 	return a+b
+// }
+// console.log(add(3,4,5))
